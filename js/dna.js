dc916aad2ccfd9a90c1c1115ed76ad0c3d80d057
DmytroBorodin <dimabor96@gmail.com>
2021-08-05 21:28:33 +0300

    implemented adjustments

--- js/dna.js
+++ js/dna.js
@@ -7,3 +7,39 @@ burgerBtn.addEventListener("click", () => {
   menu.classList.toggle("active");
   body.classList.toggle("locked");
 });
+
+const filterBtns = [...document.querySelectorAll(".filter__btn")];
+const filteredElems = [...document.querySelectorAll(".help__block")];
+
+let filters = [];
+let filterStr = "";
+
+filterBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("active");
+    let value = `${btn.querySelector("span").innerText} `;
+    if (btn.classList.contains("active")) {
+      filters.push(value);
+      filterStr = filters.join(" ");
+    } else {
+      filters.splice(filters.indexOf(value), 1);
+      filterStr = filters.join(" ");
+    }
+    filterBlocks(filterStr, filteredElems);
+  });
+});
+
+function filterBlocks(filters, blocksArr) {
+  blocksArr.forEach((block) => {
+    let value = block.getAttribute("value");
+    if (!filters.length) {
+      block.style.display = "flex";
+    } else {
+      if (filters.includes(value)) {
+        block.style.display = "flex";
+      } else {
+        block.style.display = "none";
+      }
+    }
+  });
+}
