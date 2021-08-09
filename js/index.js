471e09fb9b72880314578ff91fa01e33bc7e44a2
DmytroBorodin <dimabor96@gmail.com>
2021-08-09 11:50:31 +0300

    added app silde block

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const appWrapper = document.querySelector(".app__wrapper");
+const opentBtn = document.querySelector(".open__btn");
+const body = document.querySelector("body");
+
+console.log(document.innerWidth);
+
+opentBtn.addEventListener("click", () => {
+  let width = window.innerWidth;
+  console.log(width);
+  if (window.innerWidth <= 375) {
+    console.log(window.innerWidth);
+    body.classList.toggle("locked");
+    opentBtn.classList.toggle("active");
+    appWrapper.classList.toggle("active");
+  } else {
+    opentBtn.classList.toggle("active");
+    appWrapper.classList.toggle("active");
+  }
+});
