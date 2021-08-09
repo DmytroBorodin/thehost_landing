b40e9963d84095974cf3cc3b3b4c33df06444ac5
DmytroBorodin <dimabor96@gmail.com>
2021-08-09 12:08:58 +0300

    mobile scrolling fixes

--- js/index.js
+++ js/index.js
@@ -1,15 +1,13 @@
 const appWrapper = document.querySelector(".app__wrapper");
 const opentBtn = document.querySelector(".open__btn");
-const body = document.querySelector(".body");
-
-console.log(document.innerWidth);
+const body = document.querySelector("body");
+const bodyBlock = document.querySelector(".body");
 
 opentBtn.addEventListener("click", () => {
-  let width = window.innerWidth;
-  console.log(width);
   if (window.innerWidth <= 375) {
     console.log(window.innerWidth);
     body.classList.toggle("locked");
+    bodyBlock.classList.toggle("locked");
     opentBtn.classList.toggle("active");
     appWrapper.classList.toggle("active");
   } else {
